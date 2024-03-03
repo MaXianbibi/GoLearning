71cb14473dfab5d7804bab19c44d9f077a7108fc
Justin Morneau <jmorneau25@gmail.com>
2024-03-02 20:50:00 -0500

    Refactor routes to separate file

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -21,20 +21,9 @@ func main() {
         logger: logger,
     }
 
-    mux := http.NewServeMux()
-
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-    
-    // Swap the route declarations to use the application struct's methods as the
-    // handler functions.
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snippet/view", app.snippetView)
-    mux.HandleFunc("/snippet/create", app.snippetCreate)
-
     logger.Info("starting server", "addr", *addr)
     
-    err := http.ListenAndServe(*addr, mux)
+    err := http.ListenAndServe(*addr, app.routes())
     logger.Error(err.Error())
     os.Exit(1)
 }
